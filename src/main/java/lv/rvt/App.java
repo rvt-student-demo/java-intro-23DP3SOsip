708e2aaf2043df0e09f1c70a7f22830eae778d9f
23DP3SOsip <166009102+23DP3SOsip@users.noreply.github.com>
2024-11-22 09:09:01 +0000

    Constructor Overload

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -613,38 +613,48 @@ public class App
         // System.out.println("Matt: "+mattsCard);
 
 
-        //Book
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Book> books = new ArrayList<>();
+                                                                                    //Book
+        // Scanner scanner = new Scanner(System.in);
+        // ArrayList<Book> books = new ArrayList<>();
         
-        while (true) {
-            System.out.print("Title: ");
-            String title = scanner.nextLine();
-                if (title.isEmpty()) {
-                    break;
-        }
+        // while (true) {
+        //     System.out.print("Title: ");
+        //     String title = scanner.nextLine();
+        //         if (title.isEmpty()) {
+        //             break;
+        // }
         
-        System.out.print("Pages: ");
-        int pages = Integer.parseInt(scanner.nextLine());
+        // System.out.print("Pages: ");
+        // int pages = Integer.parseInt(scanner.nextLine());
         
-        System.out.print("Publication year: ");
-        int publicationYear = Integer.parseInt(scanner.nextLine());
+        // System.out.print("Publication year: ");
+        // int publicationYear = Integer.parseInt(scanner.nextLine());
         
-        books.add(new Book(title, pages, publicationYear));
-        }
+        // books.add(new Book(title, pages, publicationYear));
+        // }
         
-        System.out.print("What information will be printed? ");
-        String choice = scanner.nextLine();
+        // System.out.print("What information will be printed? ");
+        // String choice = scanner.nextLine();
         
-        if (choice.equals("everything")) {
-            for (Book book : books) {
-                System.out.println(book);
-            }
-        } else if (choice.equals("name")) {
-            for (Book book : books) {
-                System.out.println(book.getTitle());
-            }
-        }
+        // if (choice.equals("everything")) {
+        //     for (Book book : books) {
+        //         System.out.println(book);
+        //     }
+        // } else if (choice.equals("name")) {
+        //     for (Book book : books) {
+        //         System.out.println(book.getTitle());
+        //     }
+        // }
+
+
+                                                                            // Constructor Overload
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
+
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
     }
         //     #1
         //     public static void printText(int times) {
--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -4,6 +4,8 @@ public class Product {
     private double initialPrice;
     private String initialName;
     private int initialQuantity;
+    private String initialLocation;
+    private int initialWeight;
 
     public Product(String initialName, double initialPrice, int initialQuantity) {
         this.initialName = initialName;
@@ -11,7 +13,30 @@ public class Product {
         this.initialQuantity = initialQuantity;
     }
 
+    public Product(String name) {
+        this.initialName = name;
+        this.initialWeight = 1;
+        this.initialLocation = "shelf";
+    }
+
+    public Product(String name, String location) {
+        this.initialName = name;
+        this.initialLocation = location;
+        this.initialWeight = 1;
+    }
+
+    public Product(String name, int weight) {
+        this.initialName = name;
+        this.initialWeight = weight;
+        this.initialLocation = "shelf";
+    }
+
     public void printProduct() {
         System.out.println(this.initialName + ", price: " + this.initialPrice +", "+ this.initialQuantity + " pcs");
     }
+
+    public String toString() {
+        return this.initialName + " ("+ this.initialWeight+ " kg) can be found from the " + this.initialLocation;
+    }
+
 }
